4d11939c239ae4e80e9e069f7ce29c259e080560
smoroa14 <robertscho1240@gmail.com>
2018-09-17 09:05:53 +0200

    ASD

--- src/test/MovieTester.java
+++ src/test/MovieTester.java
@@ -5,62 +5,10 @@
  */
 package test;
 
-import com.sun.org.apache.bcel.internal.generic.AALOAD;
-import java.time.LocalDate;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import pojos.Movie;
-import pojos.MoviePK;
-
-/**
- *
- * @author rober
- */
 public class MovieTester {
 
-    private EntityManager em;
-    private EntityManagerFactory emf;
-    private Movie m;
-    private Movie m2;
-
-    public void connect() {
-        emf = Persistence.createEntityManagerFactory("MoviePU");
-        em = emf.createEntityManager();
-    }
-    
-    public void insertData()
-    {
-        m = new Movie("Alf", "The Desk", "Horror", 2024, (long) 1.5*60*60*1000);
-        em.getTransaction().begin();
-        em.persist(m);
-        em.getTransaction().commit();
-    }
-    
-    public void showData()
-    {
-        //Movie c = em.find(Movie.class, new MoviePK("testtitle", LocalDate.now().minusYears(30)));
-        //System.out.println(c);
-    }
-    
-    public void removeData()
-    {
-        em.remove(m);
-        em.getTransaction().begin();
-        em.getTransaction().commit();
-    }
-
-    public void disconnect() {
-        em.close();
-        emf.close();
-    }
 
     public static void main(String[] args) {
-        MovieTester c = new MovieTester();
-        c.connect();
-        c.insertData();
-        //c.showData();
-        //c.removeData();
-        c.disconnect();
+        System.out.println("hi");
     }
 }
